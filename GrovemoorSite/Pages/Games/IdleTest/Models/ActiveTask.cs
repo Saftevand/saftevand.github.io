adce72df237bb3b6fde59d7fd2394cb8a9daf4f4
Mathias Lundhede Hansen <mha@sik.dk>
2025-09-09 10:06:04 +0200

    repeatable tasks

--- GrovemoorSite/Pages/Games/IdleTest/Models/ActiveTask.cs
+++ GrovemoorSite/Pages/Games/IdleTest/Models/ActiveTask.cs
@@ -1,9 +1,10 @@
 namespace GrovemoorSite.Pages.Games.IdleTest.Models;
 
-public class ActiveTask(string title, int ticksTotal, Action onTaskComplete)
+public class ActiveTask(string title, int ticksTotal, Action onTaskComplete,bool repeatable = true)
 {
     public string Title { get; set; } = title;
     public int TicksTotal { get; set; } = ticksTotal;
     public int TickCounter { get; set; } = ticksTotal;
     public Action OnTaskComplete { get; set; } = onTaskComplete;
+    public bool Repeatable { get; set; } = repeatable;
 }
