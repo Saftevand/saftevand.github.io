4b0fbf1a15a75beb49e2e4f4b4334e792862c868
Saftevand <Saftevand@users.noreply.github.com>
2025-09-09 11:52:52 +0000

    deploy: bc96c43da58eb5c7f064d28da90d9c9ea411d336

---
+++ js/app.js
@@ -0,0 +1,11 @@
+﻿window.downloadFileFromByteArray = (filename, contentType, byteArray) => {
+    const blob = new Blob([byteArray], { type: contentType });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+};
