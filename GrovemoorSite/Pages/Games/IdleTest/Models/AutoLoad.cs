954ee3e3d03c696c6d0d139cdea4b83c50533c79
Mathias Lundhede Hansen <mha@sik.dk>
2025-09-19 14:07:37 +0200

    Market

--- GrovemoorSite/Pages/Games/IdleTest/Models/AutoLoad.cs
+++ GrovemoorSite/Pages/Games/IdleTest/Models/AutoLoad.cs
@@ -4,8 +4,8 @@
     {
         public static Dictionary<ItemType, Item> Items = new()
         {
-            { ItemType.Seed, new Item(ItemType.Seed, "Seed", 1, "potted_plant", 0) },
-            { ItemType.Tomato, new Item(ItemType.Tomato, "Tomato", 5, "nutrition", 0, 200) }
+            { ItemType.Seed, new Item(ItemType.Seed, "Seed", 1, "potted_plant", 1) },
+            { ItemType.Tomato, new Item(ItemType.Tomato, "Tomato", 5, "nutrition", 5, 20) }
         };
     }
 }
